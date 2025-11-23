1845a3a61b57b3818e421e8f0cbb4bb48f6102f9
Stephen Oravec <81935688+StephenOravec@users.noreply.github.com>
2025-11-23 14:58:43 -0500

    Add tool to agent

--- Chapter3/customer_service_agent_v2.py
+++ Chapter3/customer_service_agent_v2.py
@@ -1,22 +1,16 @@
 import os
 from dotenv import load_dotenv
 from agents import Agent, Runner, function_tool
-# Load environment variables from the.env file
+
+# Load environment variables from the .env file
 load_dotenv()
 # Access the API key
 api_key = os.getenv("OPENAI_API_KEY")
-# Check to confirm API key is accessible:
-if not api_key:
-    print("Error: OPENAI_API_KEY not found. Please set it in your.env file.")
-else:
-    print("API Key loaded successfully.")
 
 # Create a tool
 @function_tool
 def get_order_status(orderID: int) -> str:
-    """
-    Returns the order status given an order ID
-    """
+    """Returns the order status given an order ID"""
     if orderID in (100, 101):
         return "Delivered"
     elif orderID in (200, 201):
@@ -24,7 +18,12 @@ def get_order_status(orderID: int) -> str:
     elif orderID in (300, 301):
         return "Cancelled"
 
-#Create an agent and run it
-agent = Agent(name="Echo Agent", instructions="Return the words 'Setup successful'")
-result = Runner.run_sync(agent, "Run setup")
+# Define an agent
+agent = Agent(name="Customer service agent",
+              instructions="You are an AI Agent that helps respond to customer queries for a local paper company",
+              model="gpt-4o")
+
+# Run the Control Logic Framework
+result = Runner.run_sync(agent, "How do I cancel my order?")
+# Print the result
 print(result.final_output)
