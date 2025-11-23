49be3e42d27553412b7b0849ef3240d6e29be0f8
Stephen Oravec <81935688+StephenOravec@users.noreply.github.com>
2025-11-23 15:00:15 -0500

    Add tools to agent definition

--- Chapter3/customer_service_agent_v2.py
+++ Chapter3/customer_service_agent_v2.py
@@ -21,7 +21,8 @@ def get_order_status(orderID: int) -> str:
 # Define an agent
 agent = Agent(name="Customer service agent",
               instructions="You are an AI Agent that helps respond to customer queries for a local paper company",
-              model="gpt-4o")
+              model="gpt-4o"
+              tools=[get_order_status])
 
 # Run the Control Logic Framework
 result = Runner.run_sync(agent, "How do I cancel my order?")
